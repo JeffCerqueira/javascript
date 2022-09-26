9e2f14311d1bfbaf8a7ebefdbb05940e68b16e46
Jefeson Cerqueira <jeffcerqueira99@gmail.com>
2022-09-26 11:30:20 -0300

    estudos 26/09

---
+++ aula11/ex008.js
@@ -0,0 +1,5 @@
+var vel = 70
+console.log('O console funcionou carai de asa')
+if(vel > 60){
+    console.log(`Você ultrapassou o limite de velocidade, chegando a ${vel}km/h, MULTADO !`)
+}
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'EUA'
+console.log(`Vivendo em ${pais}`)
+if(pais == ('brasil')){
+    console.log('Você é Brasileiro, de volta a casa')
+} else {
+    console.log('Você é estrangeiro, seja bem vindo')
+}
