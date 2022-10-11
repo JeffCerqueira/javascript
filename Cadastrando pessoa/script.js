7bb9e54b5366df6856c559813414d41211f9002e
Jefeson Cerqueira <jeffcerqueira99@gmail.com>
2022-10-11 14:17:49 -0300

    11/10

--- Cadastrando pessoa/script.js
+++ Cadastrando pessoa/script.js
@@ -17,9 +17,26 @@ function verificar() {
                 img.setAttribute('src', 'menino.jpeg')
             } else if (idade < 21) {
                 img.setAttribute('src', 'foto-jovem.webp')
+            } else if (idade < 45) {
+                img.setAttribute('src', 'adulto-homem.jpg')
+            } else if(idade < 90) {
+                img.setAttribute('src', 'idoso.jpeg')
+            } else {
+                alert('Erro ! tente novamente')
             }
         } else if (sex[1].checked) {
             genero = 'Mulher'
+            if(idade >= 0 && idade < 10) {
+                img.setAttribute('src', 'menina.jpeg')
+            } else if (idade < 21) {
+                img.setAttribute('src', 'jovem-mulher.jpeg')
+            } else if (idade < 45) {
+                img.setAttribute('src', 'adulto-mulher.jpeg')
+            } else if (idade < 90) {
+                img.setAttribute('src', 'idosa.jpeg')
+            } else {
+                alert('Valor incopativel, tente novamente !')
+            }
         }
     }
     res.style.textAlign = 'center'
---
+++ aula13/ambiente.js
@@ -0,0 +1,10 @@
+var cont = Number(0)
+do {
+    console.log('Tudo bem ?')
+    cont ++
+} while (cont < 6)
+
+//while (cont < 10) {
+//    console.log('Tudo bem ?')
+//    cont ++
+//} 
